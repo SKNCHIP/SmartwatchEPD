53ba26ab283b63cef65b142e8971b3b7945581a6
Grzegorz Orlicki <grzorl2907@gmail.com>
2019-03-10 23:55:34 +0100

    Day 1 - untested lib

---
+++ SmartwatchEPD/Inc/EPDlib.h
@@ -0,0 +1,91 @@
+/*
+    EPDlib.h
+    Created by SKN CHIP (Grzegorz Orlicki)
+    Based on the documentation for E-paper display USE0154BS01
+    MIT License 
+*/
+
+#ifndef EPDlib_H_
+#define EPDlib_H_
+
+//Constants
+#define RESET 0x12
+#define COL 152
+#define ROW 19 //152/8
+//-Begin
+#define AnalogBlockControl 0x74
+#define DigitalBlockControl 0x7E
+#define DisplayOutputControl 0x01
+#define RAMDataEntry 0x11
+#define RAMX 0x44
+#define RAMY 0x45
+#define TemperatureSelection 0x18
+#define Border 0x3C
+
+#define VCOM 0x2C
+#define GateVoltage 0x03
+#define SourceVoltage 0x04
+#define Frame1 0x3A
+#define Frame2 0x3B
+#define LoadLUT 0x32
+
+#define RAMXCounter 0x4E
+#define RAMYCounter 0x4F
+#define LoadImage 0x24
+
+#define Update1 0x22
+#define Update2 0x20
+#define DeepSleepMode 0x10
+
+const uint8_t LUTTable[]={
+		0x00,
+		0xA5,
+		0x66,
+		0x66,
+		0x6A,
+		0x66,
+		0x9A,
+		0x18,
+		0x24,
+		0x66,
+		0x00,
+		0x55,
+		0x99,
+		0x11,
+		0x88,
+		0x11,
+		0x88,
+		0x88,
+		0x11,
+		0x88,
+		0xF0,
+		0xFF,
+		0xFF,
+		0xFF,
+		0xFF,
+		0xF2,
+		0xFF,
+		0xFF,
+		0xAF,
+		0x5F,
+                     // VGH  VSH1  VSH2 VSL   0X3A 0X3B(Ƶ��)
+};
+
+//Declarations
+//-Communication
+void EPDlib_SendCommand(uint8_t command);
+void EPDlib_SendData(uint8_t data);
+//-Basic
+void EPDlib_HardwareReset();
+void EPDlib_SoftwareReset();
+void EPDlib_WaitforEPD();
+    //void EPDlib_Begin();
+//-Display
+void EPDlib_Start();
+
+void EPDlib_Image(const uint8_t* image);
+void EPDlib_White();
+void EPDlib_Black();
+
+void EPDlib_Update();
+#endif
---
+++ SmartwatchEPD/Src/EPDlib.c
@@ -0,0 +1,152 @@
+/*
+    EPDlib.c
+    Created by SKN CHIP (Grzegorz Orlicki)
+    Based on the documentation for E-paper display USE0154BS01
+    MIT License
+*/
+
+#include "main.h"
+#include "stm32f0xx_hal.h"
+#include "spi.h"
+#include "gpio.h"
+#include "EPDlib.h"
+
+//Communication
+void EPDlib_SendCommand(uint8_t command){
+    uint8_t temp = command;
+
+    HAL_GPIO_WritePin(EPD_CS_GPIO_Port, EPD_CS_Pin, 0);
+	HAL_GPIO_WritePin(EPD_DC_GPIO_Port, EPD_DC_Pin, 0);
+	HAL_SPI_Transmit(&hspi1, &temp, sizeof(temp), 10);
+	HAL_GPIO_WritePin(EPD_CS_GPIO_Port, EPD_CS_Pin, 1);
+}
+void EPDlib_SendData(uint8_t data){
+    uint8_t temp = data;
+
+	HAL_GPIO_WritePin(EPD_CS_GPIO_Port, EPD_CS_Pin, 0);
+	HAL_GPIO_WritePin(EPD_DC_GPIO_Port, EPD_DC_Pin, 1);
+	HAL_SPI_Transmit(&hspi1, &temp, sizeof(temp), 10);
+	HAL_GPIO_WritePin(EPD_CS_GPIO_Port, EPD_CS_Pin, 1);
+}
+
+//Basic
+void EPDlib_HardwareReset(){
+    HAL_GPIO_WritePin(EPD_RESET_GPIO_Port, EPD_RESET_Pin, 0);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(EPD_RESET_GPIO_Port, EPD_RESET_Pin, 1);
+	HAL_Delay(1);
+}
+void EPDlib_SoftwareReset(){
+    EPDlib_SendCommand(RESET);
+}
+void EPDlib_WaitforEPD(){
+    while(1){
+		if(!HAL_GPIO_ReadPin(EPD_BUSY_GPIO_Port, EPD_BUSY_Pin)) break;
+	}
+}
+
+//Display
+void EPDlib_Start(){
+    //Power On
+    EPDlib_HardwareReset();
+    EPDlib_WaitforEPD();
+    EPDlib_SoftwareReset();
+    EPDlib_WaitforEPD();
+
+    EPDlib_SendCommand(AnalogBlockControl);
+    EPDlib_SendData(0x54);
+    EPDlib_SendCommand(DigitalBlockControl);
+    EPDlib_SendData(0x3B);
+
+    EPDlib_SendCommand(DisplayOutputControl);
+    EPDlib_SendData(0x97);
+    EPDlib_SendData(0x00);
+    EPDlib_SendData(0x00);
+
+    EPDlib_SendCommand(RAMDataEntry);
+    EPDlib_SendData(0x01);
+    EPDlib_SendCommand(RAMX);
+    EPDlib_SendData(0x00);
+    EPDlib_SendData(0x12);
+    EPDlib_SendCommand(RAMY);
+    EPDlib_SendData(0x97);
+    EPDlib_SendData(0x00);
+    EPDlib_SendData(0x00);
+    EPDlib_SendData(0x00);
+
+    EPDlib_SendCommand(TemperatureSelection); //uwaga dodane
+    EPDlib_SendData(0x80);
+    EPDlib_SendCommand(Border);
+    EPDlib_SendData(0x01);
+
+    //Set voltage and load LUT
+    EPDlib_SendCommand(VCOM);
+    EPDlib_SendData(0x52);
+    EPDlib_SendCommand(GateVoltage);
+    EPDlib_SendData(0x17);
+    EPDlib_SendCommand(SourceVoltage);
+    EPDlib_SendData(0x41);
+    EPDlib_SendData(0x00);
+    EPDlib_SendData(0x32);
+
+    EPDlib_SendCommand(Frame1);
+    EPDlib_SendData(0x11);
+    EPDlib_SendCommand(Frame2);
+    EPDlib_SendData(0x0D);
+    EPDlib_SendCommand(LoadLUT);
+    for(i = 0; i < 30; i++) EPD_SendData(LUTTable[i]);
+}
+void EPDlib_Image(const uint8_t* image){
+    EPDlib_SendCommand(RAMXCounter);
+    EPDlib_SendData(0x00);
+    EPDlib_SendCommand(RAMYCounter);
+    EPDlib_SendData(0x97);
+    EPDlib_SendData(0x00);
+
+    EPDlib_SendCommand(LoadImage);
+    uint32_t counter = 0;
+    for(uint8_t i = 0; i < COL; i++){
+        for(uint8_t j = 0; j < ROW; j++){
+            EPDlib_SendData(image[counter++]);
+        }
+    }
+}
+void EPDlib_White(){
+    EPDlib_SendCommand(RAMXCounter);
+    EPDlib_SendData(0x00);
+    EPDlib_SendCommand(RAMYCounter);
+    EPDlib_SendData(0x97);
+    EPDlib_SendData(0x00);
+
+    EPDlib_SendCommand(LoadImage);
+    uint32_t counter = 0;
+    for(uint8_t i = 0; i < COL; i++){
+        for(uint8_t j = 0; j < ROW; j++){
+            EPDlib_SendData(0xFF);
+        }
+    }
+}
+void EPDlib_Black(){
+    EPDlib_SendCommand(RAMXCounter);
+    EPDlib_SendData(0x00);
+    EPDlib_SendCommand(RAMYCounter);
+    EPDlib_SendData(0x97);
+    EPDlib_SendData(0x00);
+
+    EPDlib_SendCommand(LoadImage);
+    uint32_t counter = 0;
+    for(uint8_t i = 0; i < COL; i++){
+        for(uint8_t j = 0; j < ROW; j++){
+            EPDlib_SendData(0x00);
+        }
+    }
+}
+void EPDlib_Update(){
+    EPDlib_SendCommand(Update1);
+    EPDlib_SendData(0xC7);
+    EPDlib_SendCommand(Update2);
+
+    EPDlib_WaitforEPD();
+    EPDlib_SendCommand(DeepSleepMode);
+    EPDlib_SendData(0x01);
+}
